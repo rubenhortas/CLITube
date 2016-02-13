6b5d89e09bc735d60d15433bb13e60ffdd43b84a
rubenhortas <rubenhortas@gmail.com>
2016-02-13 12:26:56 +0100

    Some work done

--- application/clitube.py
+++
@@ -1,9 +0,0 @@
-"""
-@author:  Rubén Hortas Astariz <http://rubenhortas.blogspot.com>
-@contact: rubenhortas at gmail.com
-@github:  http://github.com/rubenhortas
-@license: CC BY-NC-SA 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/>
-@file:    clitube  
-"""
-
-
