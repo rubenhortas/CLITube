88c984cee3a1f8c753dac021132a21fe0a7b6bf2
Rubén <ruben.hortas@gmail.com>
2017-02-28 20:04:06 +0100

    Fixed bug

--- domain/mplayer.py
+++ domain/mplayer.py
@@ -12,7 +12,7 @@ from .video_player import VideoPlayer
 
 class Mplayer(VideoPlayer):
     name = "mplayer"
-    flags = "-fs -really-quiet -cookies -cookies-file {1}".format(COOKIE_FILE)
+    flags = "-fs -really-quiet -cookies -cookies-file {0}".format(COOKIE_FILE)
 
     def _get_command(self):
         return "{0} -fs -really-quiet -cookies -cookies-file".format(self.name)
