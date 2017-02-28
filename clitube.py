e6fc9c8c0c4dd63d8253551a94b01ac6073b7dbf
Rubén <ruben.hortas@gmail.com>
2017-02-28 17:46:36 +0100

    Improved

--- clitube.py
+++ clitube.py
@@ -7,6 +7,7 @@
 """
 
 import argparse
+import re
 import signal
 
 from application.utils.python_utils import exit_signal_handler
@@ -18,6 +19,27 @@ from crosscutting.constants import REQUIRED_PYTHON_VERSION
 from domain.video_player_factory import get_instance_of
 from domain.youtubedl import Youtubedl
 from presentation.utils.screen import clear_screen
+
+YOUTUBE_PATTERN = re.compile("(http(s)?://www.youtube.com/watch\?v=(\w)*)")
+URL_PATTERN = re.compile(
+    "(?P<video_url>http(s)?://www.youtube.com/watch\?v=(\w)*)&list=(?P<list>(\w)*)&index=(?P<index>(\d)*)")
+
+
+def __is_youtube(url):
+    match = YOUTUBE_PATTERN.search(url)
+    if match:
+        return True
+    else:
+        return False
+
+
+def __clean_url(url):
+    match = URL_PATTERN.search(url)
+    if match:
+        return match.group(0)
+    else:
+        return url
+
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, exit_signal_handler)
@@ -33,16 +55,20 @@ if __name__ == "__main__":
         parser.add_argument("youtube_url", metavar="YOUTUBE_VIDEO_URL", nargs=1, help="URL of the video on youtube.")
         args = parser.parse_args()
 
-        youtube_url = args.youtube_url[0]
+        user_url = args.youtube_url[0]
 
-        youtubedl = Youtubedl(youtube_url)
-        video_player = get_instance_of(VIDEO_PLAYER)
+        if __is_youtube(user_url):
+            youtube_url = __clean_url(user_url)
 
-        print_header()
-        print_fetching(youtube_url)
-        url = youtubedl.get_url()
-        video_player.play(url)
+            youtubedl = Youtubedl(user_url)
+            video_player = get_instance_of(VIDEO_PLAYER)
 
+            print_header()
+            print_fetching(user_url)
+            video_url = youtubedl.get_url()
+            video_player.play(video_url)
+        else:
+            print_error('Is not a youtube video')
     else:
         print_error('Requires Python {0}'.format(REQUIRED_PYTHON_VERSION))
         exit(0)
